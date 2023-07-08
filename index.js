08c355d406336eabd63b3dc24b29b173e634808f
MOHD SABIR <sabiransari01981@gmail.com>
2023-07-08 16:43:53 +0530

    adding home route

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(seatsRouter);   // to use routes 
 
+app.get('/',async(req,res)=>{
+res.send("<div style='background-color : black'>  <h1 style='text-align : center; color:white'> Welcome To Seat Booking API </h1><p  style='text-align : center; color:white'> It is an API that is used to Display the working of Seat Booking. I have made 3 type pf classes['First,Business,Economy'] for booking the seats.<p> </div>");
+})
 
 
 // Start the server
